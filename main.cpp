31252e89defd62bab73e724e7bc1f1fb35ce84c6
Corji4 <beingevilhasaprice>
2019-03-14 12:25:06 +0300

    изменена последовательность записи файла

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ struct students
 void print_list_txt(students* list, int number, ofstream& output);
 void print_list_bin(students* list, int number, ofstream& output);
 void create(students list[], int number_of_students);
-void print_to_console(string name);
+void get_new_list(string name, students list[], int number);
 string get_name(string gender);
 
 int main()
@@ -28,7 +28,8 @@ int main()
 	setlocale(LC_ALL, "RUS");
 	srand(time(NULL));
 	students list[100];
-	int number_of_students = 1 + rand() % 100;
+	students bad_list[100];
+	int number_of_students = 5;//1 + rand() % 100;
 	create(list, number_of_students);
 	ofstream all_students_bin("all_students.bin", ios::binary);
 	for (int i = 0; i < number_of_students; i++)
@@ -36,16 +37,18 @@ int main()
 		print_list_bin(list, i, all_students_bin);
 	}
 	all_students_bin.close();
-	ofstream bad_students_txt("bad_students.txt");
+	ofstream bad_students_bin("bad_students.bin",ios::binary);
+	int bad_number=0;
 	for (int i = 0; i < number_of_students; i++)
 	{
 		if (list[i].progress < 4)
 		{
-			print_list_txt(list, i, bad_students_txt);
+			print_list_bin(list, i, bad_students_bin);
+			bad_number++;
 		}
 	}
-	bad_students_txt.close();
-	print_to_console("bad_students.txt");
+	bad_students_bin.close();
+	get_new_list("bad_students.bin", bad_list, bad_number);
 	system("pause");
 	return 0;
 }
@@ -104,54 +107,50 @@ void print_list_txt(students* list, int number, ofstream& output)
 
 void print_list_bin(students* list, int number, ofstream& output)
 {
-	string name = (list + number)->name2;
-	output.write(reinterpret_cast <char*> (&name), sizeof(string));
-	name = (list + number)->name1;
-	output.write(reinterpret_cast <char*> (&name), sizeof(string));
-	name = (list + number)->name3;
-	output.write(reinterpret_cast <char*> (&name), sizeof(string));
+	string name1, name2, name3;
+	name2= (list + number)->name2;
+	name1= (list + number)->name1;
+	name3= (list + number)->name3;
 	int age = (list + number)->age;
-	output.write(reinterpret_cast <char*> (&age), sizeof(int));
 	string gender = (list + number)->gender;
-	output.write(reinterpret_cast <char*> (&gender), sizeof(string));
 	int course = (list + number)->course;
-	output.write(reinterpret_cast <char*> (&course), sizeof(int));
 	double progress = (list + number)->progress;
+	output.write(reinterpret_cast <char*> (&name2), sizeof(string));
+	output.write(reinterpret_cast <char*> (&name1), sizeof(string));
+	output.write(reinterpret_cast <char*> (&name3), sizeof(string));
+	output.write(reinterpret_cast <char*> (&age), sizeof(int));
+	output.write(reinterpret_cast <char*> (&gender), sizeof(string));
+	output.write(reinterpret_cast <char*> (&course), sizeof(int));
 	output.write(reinterpret_cast <char*> (&progress), sizeof(double));
 }
 
-void print_to_console(string file_name)
+void get_new_list(string name, students list[], int number)
 {
-	ifstream input(file_name);
-	string name1, name2, name3;
-	input >> name2;
-	input >> name1;
-	input >> name3;
+	ifstream input(name,ios::binary);
+	input.clear();
+	string name1;
+	string name2;
+	string name3;
 	int age;
-	input >> age;
 	string gender;
-	input>>gender;
 	int course;
-	input >> course;
 	double progress;
-	input >> progress;
-	while (!input.eof())
+	for (int i = 0; i < number; i++)
 	{
-		cout << setw(15) << name2;
-		cout << setw(15) << name1;
-		cout << setw(20) << name3;
-		cout << setw(5) << age;
-		cout << setw(10) << gender;
-		cout << setw(5) << course;
-		cout << setw(5) << progress;
-		cout << endl;
-		input >> name2;
-		input >> name1;
-		input >> name3;
-		input >> age;
-		input >> gender;
-		input >> course;
-		input >> progress;
+		input.read(reinterpret_cast <char*> (&name2), sizeof(string));
+		input.read(reinterpret_cast <char*> (&name1), sizeof(string));
+		input.read(reinterpret_cast <char*> (&name3), sizeof(string));
+		input.read(reinterpret_cast <char*> (&age), sizeof(int));
+		input.read(reinterpret_cast <char*> (&gender), sizeof(string));
+		input.read(reinterpret_cast <char*> (&course), sizeof(int));
+		input.read(reinterpret_cast <char*> (&progress), sizeof(double));
+		(list + i)->name1 = name1;
+		(list + i)->name2 = name2;
+		(list + i)->name3 = name3;
+		(list + i)->age = age;
+		(list + i)->gender = gender;
+		(list + i)->course = course;
+		(list + i)->progress = progress;
 	}
 	input.close();
 }
